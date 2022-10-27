62143eb1b62886e428e5fd7a785b6ee9af56bdac
Parth442002 <parthkatiyar04@gmail.com>
2022-10-28 02:43:44 +0530

    Project Created

---
+++ Api/config.py
@@ -0,0 +1,15 @@
+from pydantic import BaseSettings
+
+#Least Recently Used Caching
+from functools import lru_cache
+
+class Settings(BaseSettings):
+    env_name: str = "Local"
+    base_url: str = "http://localhost:8000"
+    db_url: str = "sqlite:///./shortener.db"
+
+@lru_cache
+def get_settings() -> Settings:
+    settings = Settings()
+    print(f"Loading settings for: {settings.env_name}")
+    return settings
