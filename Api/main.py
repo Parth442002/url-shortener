4511b31430ad4af925839a36151c6ae64f1fc17a
Parth442002 <parthkatiyar04@gmail.com>
2022-10-28 11:58:50 +0530

    no. of links functionality added complete

--- Api/main.py
+++ Api/main.py
@@ -80,3 +80,14 @@ def get_admin_info(db_url: models.URL) -> schemas.URLInfo:
     db_url.url = str(base_url.replace(path=db_url.key))
     db_url.admin_url = str(base_url.replace(path=admin_endpoint))
     return db_url
+
+
+@app.delete("/admin/{secret_key}")
+def delete_url(
+    secret_key: str, request: Request, db: Session = Depends(get_db)
+):
+    if db_url := crud.deactivate_db_url_by_secret_key(db, secret_key=secret_key):
+        message = f"Successfully deleted shortened URL for '{db_url.target_url}'"
+        return {"detail": message}
+    else:
+        raise_not_found(request)
