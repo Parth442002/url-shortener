11c8d1b2e4bdc69fadce033016862f134db3b67c
Parth442002 <parthkatiyar04@gmail.com>
2022-10-28 11:58:17 +0530

    no. of links functionality added

--- Api/main.py
+++ Api/main.py
@@ -2,12 +2,14 @@ import validators
 from sqlalchemy.orm import Session
 from fastapi import Depends, FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
+from starlette.datastructures import URL
 
 import secrets
 from . import schemas
 from . import models, schemas
 from .database import SessionLocal, engine
 from . import crud, models, schemas
+from .config import get_settings
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
@@ -39,12 +41,8 @@ def raise_not_found(request):
 def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     if not validators.url(url.target_url):
         raise_bad_request(message="Your provided URL is not valid")
-
     db_url = crud.create_db_url(db=db, url=url)
-    db_url.url = db_url.key
-    db_url.admin_url = db_url.secret_key
-
-    return db_url
+    return get_admin_info(db_url)
 
 
 @app.get("/{url_key}")
@@ -54,6 +52,31 @@ def forward_to_target_url(
     db: Session = Depends(get_db)
 ):
     if db_url := crud.get_db_url_by_key(db=db, url_key=url_key):
+        crud.update_db_clicks(db=db, db_url=db_url)
         return RedirectResponse(db_url.target_url)
     else:
         raise_not_found(request)
+
+
+@app.get(
+    "/admin/{secret_key}",
+    name="administration info",
+    response_model=schemas.URLInfo,
+)
+def get_url_info(
+    secret_key: str, request: Request, db: Session = Depends(get_db)
+):
+    if db_url := crud.get_db_url_by_secret_key(db, secret_key=secret_key):
+        return get_admin_info(db_url)
+    else:
+        raise_not_found(request)
+
+
+def get_admin_info(db_url: models.URL) -> schemas.URLInfo:
+    base_url = URL(get_settings().base_url)
+    admin_endpoint = app.url_path_for(
+        "administration info", secret_key=db_url.secret_key
+    )
+    db_url.url = str(base_url.replace(path=db_url.key))
+    db_url.admin_url = str(base_url.replace(path=admin_endpoint))
+    return db_url
