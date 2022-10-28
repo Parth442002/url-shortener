eb26739d82e98a804f663db797b694b18e1bc6f9
Parth442002 <parthkatiyar04@gmail.com>
2022-10-28 12:46:16 +0530

    setup client application

---
+++ client/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function index() {
+  return (
+    <main className="h-full w-full flex-1">
+      <div>Parth Katiyar</div>
+    </main>
+  )
+}
+
+export default index
