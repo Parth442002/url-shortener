40931217793c7c2c0ff050fbbb5352a5fb23acad
Parth442002 <parthkatiyar04@gmail.com>
2023-11-09 23:30:47 +0530

    Added Docker Script

---
+++ createContainer.sh
@@ -0,0 +1,6 @@
+echo "Running Docker Build Script"
+
+docker images -a | grep 'parth404/shortner:1.0' | awk '{print $3}' | xargs docker rmi -f
+docker build --platform linux/amd64 --tag parth404/shortner:1.0 .
+docker push parth404/upsplit_server:latest
+echo "Process Completed"
