d41b4cce8f71610220e53f0c0daa612af6db9db9
Parth442002 <parthkatiyar04@gmail.com>
2023-11-10 00:08:49 +0530

    change in host

--- createContainer.sh
+++ createContainer.sh
@@ -2,5 +2,5 @@ echo "Running Docker Build Script"
 
 docker images -a | grep 'parth404/shortner:1.0' | awk '{print $3}' | xargs docker rmi -f
 docker build --platform linux/amd64 --tag parth404/shortner:1.0 .
-docker push parth404/upsplit_server:latest
+docker push parth404/shortner:1.0
 echo "Process Completed-->"
