9d5b577d1f90aaa37c506f6e859cfbb01c9303aa
Parth442002 <parthkatiyar04@gmail.com>
2023-11-09 23:58:54 +0530

    minor update

--- createContainer.sh
+++ createContainer.sh
@@ -3,4 +3,4 @@ echo "Running Docker Build Script"
 docker images -a | grep 'parth404/shortner:1.0' | awk '{print $3}' | xargs docker rmi -f
 docker build --platform linux/amd64 --tag parth404/shortner:1.0 .
 docker push parth404/upsplit_server:latest
-echo "Process Completed"
+echo "Process Completed-->"
